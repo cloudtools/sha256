165765c44486845477d79ce49dbab0532a38d14a
James E. Blair <corvus@inaugust.com>
2024-01-03 15:37:32 -0800

    Add support for python 3.11+ (#5)
    The cython generated C code is different for 3.11 vs 3.10, so
    relying on pip using an sdist with that file included doesn't work.
    That is to say, that "pip install sha256" doesn't work on 3.11 with
    the 0.3 release on pypi.
    
    This change updates setup.py to the current recommendation from
    cython (which removes the sdist step).  It adds a pyproject.toml
    so that cython is recognized as a build-time dependency in recent
    pip versions.  Package classifiers are updated to include 3.11
    and 3.12, and dropping the EOL 3.7. Finally the sha256 package
    version is updated to 1.0 to prepare for a future release.

--- setup.py
+++ setup.py
@@ -1,26 +1,14 @@
 from setuptools import setup
-from setuptools.extension import Extension
-from setuptools.command.sdist import sdist as _sdist
-
-cmdclass = {}
-
-
-class sdist(_sdist):
-    def run(self):
-        from Cython.Build import cythonize
-        cythonize(['sha256.pyx'])
-        _sdist.run(self)
-cmdclass['sdist'] = sdist
+from Cython.Build import cythonize
 
 
 setup(
     name='sha256',
-    version='0.3',
+    version='1.0',
     description='sha256 library with midstate',
     author='Mark Peek',
     author_email='mark@peek.org',
     license="MIT",
-    cmdclass=cmdclass,
-    ext_modules=[Extension("sha256", ["sha256.c"])],
+    ext_modules=cythonize(['sha256.pyx']),
     test_suite="tests",
 )
